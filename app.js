c021ef340ecde2cde25c21e49057b88a71e4ad38
Mostafa-shaban <dashassas@gmail.com>
2018-10-17 02:12:59 +0200

    get item in filterNames function

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // Grap input element
-let filterInput = document .getElementById('filterInput');
+let filterInput = document.getElementById('filterInput');
 
 // Add event listener
 filterInput.addEventListener('keyup', filterNames);
 
+function filterNames() {
+	// Get value of input
+	let filterValue = document.getElementById('filterInput').value.toUpperCase();
+	// Get names ul
+	let ul = document.getElementById('names');
+	// Get lis from ul
+	let li = ul.querySelectorAll('li.collection-item')
+
+}
